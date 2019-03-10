7f26d797766a03c25a3aab46829871463da2bd82
vinodhraja muralidharan <vinodhraja.muralidharan@gmail.com>
2019-03-10 21:37:32 +0000

    profile update

--- js/app.js
+++ js/app.js
@@ -7,7 +7,7 @@ app.controller("homeController", function ($scope) {
 app.directive("navigationMenu", function () {
 	var directive = {};
 	directive.restrict = 'E';
-	directive.template = '<div id="main-nav" class="collapse navbar-collapse"><ul class="nav navbar-nav" id="mainNav"><li class="active"><a href="#home" class="scroll-link">Home</a></li><li><a href="#aboutUs" class="scroll-link">About Me</a></li><li><a href="#skills" class="scroll-link">Skills</a></li><li><a href="#experience" class="scroll-link">Experience</a></li><li><a href="#portfolio" class="scroll-link">Portfolio</a></li><li><a href="#contactUs" class="scroll-link">Contact Us</a></li></ul></div>';
+	directive.template = '<div id="main-nav" class="collapse navbar-collapse"><ul class="nav navbar-nav" id="mainNav"><li class="active"><a href="#home" class="scroll-link">Home</a></li><li><a href="#aboutUs" class="scroll-link">About Us</a></li><li><a href="#skills" class="scroll-link">Skills</a></li><li><a href="#experience" class="scroll-link">Experience</a></li><li><a href="#portfolio" class="scroll-link">Portfolio</a></li><li><a href="#contactUs" class="scroll-link">Contact Me</a></li></ul></div>';
 	return directive;
 
 });
